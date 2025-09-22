732b17a10b82756a7c1216cc596c6d0d263565ec
Martin <martin.herve@etudiant.univ-rennes.fr>
2025-09-22 14:35:26 +0200

    implémentation constructeur pour Session

--- src/sunt/sqb/elements/Session.java
+++ src/sunt/sqb/elements/Session.java
@@ -1,10 +1,20 @@
 package sunt.sqb.elements;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Session {
     private int id;
     private String date;
+    private String remplisPar;
+    private float br;
     private List<Game> games;
     
+    public Session (int id, String date, String remplisPar, float br) {
+        this.id = id;
+        this.date = date;
+        this.remplisPar = remplisPar;
+        this.br = br;
+        this.games = new ArrayList();
+    }
 }
