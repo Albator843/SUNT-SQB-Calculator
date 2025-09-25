142aa4e57fe76c2af3d6818584442b452ac351eb
Martin HERVE <martin.herve@etudiant.univ-rennes.fr>
2025-09-26 00:15:06 +0200

    ajout fonction getStats qui renvoie un tableau des statistiques et ajout de commentaire dans TableStatsTypeVehicule.java

--- src/sunt/sqb/elements/TableStatsTypeVehicule.java
+++ src/sunt/sqb/elements/TableStatsTypeVehicule.java
@@ -10,6 +10,9 @@ public class TableStatsTypeVehicule {
     private int nbCaptures;
     private int nbMorts;
 
+    /*
+     * Constructeur
+     */
     public TableStatsTypeVehicule(TypeVehicule typeVehicule) {
         this.typeVehicule = typeVehicule;
         this.nbGames = 0;
@@ -21,6 +24,9 @@ public class TableStatsTypeVehicule {
         this.nbMorts = 0;
     }
 
+    /*
+     * Ajoute des statistiques
+     */
     public void addStats(int nbPoints, int nbKillsAir, int nbKillsSol, int nbAssistances, int nbCaptures, int nbMorts) {
         this.nbGames++;
         this.nbPoints += nbPoints;
@@ -31,6 +37,10 @@ public class TableStatsTypeVehicule {
         this.nbMorts += nbMorts;
     }
 
+    /*
+     * Ajoute les statistiques d'un autre TableStatsTypeVehicule
+     * Les statistiques doivent être du même type de véhicule
+     */
     public void addStats(TableStatsTypeVehicule stats) {
         if (this.typeVehicule != stats.typeVehicule) {
             throw new IllegalArgumentException("Cannot add stats of different vehicle types");
@@ -44,35 +54,73 @@ public class TableStatsTypeVehicule {
         this.nbMorts += stats.nbMorts;
     }
 
+    /*
+     * Retourne le type de véhicule
+     */
     public TypeVehicule getTypeVehicule() {
         return typeVehicule;
     }
 
+    /*
+     * Retourne le nombre de parties jouées
+     */
     public int getNbGames() {
         return nbGames;
     }
 
+    /*
+     * Retourne le nombre de points
+     */
     public int getNbPoints() {
         return nbPoints;
     }
 
+    /*
+     * Retourne le nombre de kills en air
+     */
     public int getNbKillsAir() {
         return nbKillsAir;
     }
 
+    /*
+     * Retourne le nombre de kills au sol
+     */
     public int getNbKillsSol() {
         return nbKillsSol;
     }
 
+    /*
+     * Retourne le nombre d'assistances
+     */
     public int getNbAssistances() {
         return nbAssistances;
     }
 
+    /*
+     * Retourne le nombre de captures
+     */
     public int getNbCaptures() {
         return nbCaptures;
     }
 
+    /*
+     * Retourne le nombre de morts
+     */
     public int getNbMorts() {
         return nbMorts;
+    }
+
+    /*
+     * Retourne les statistiques sous forme de tableau d'entiers
+     * [0] = nbGames
+     * [1] = nbPoints
+     * [2] = nbKillsAir
+     * [3] = nbKillsSol
+     * [4] = nbAssistances
+     * [5] = nbCaptures
+     * [6] = nbMorts
+     */
+    public int[] getStats() {
+        return new int[] {nbGames, nbPoints, nbKillsAir, nbKillsSol, nbAssistances, nbCaptures, nbMorts};
     }
 }
