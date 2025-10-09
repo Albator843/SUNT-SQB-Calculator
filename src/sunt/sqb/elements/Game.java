3f011f77fddd3a5f310d028a97fcfd14e96613ce
Martin HERVE <martin.herve@etudiant.univ-rennes.fr>
2025-10-09 22:06:20 +0200

    implémentation resumeStatsGame() pour Game.java

--- src/sunt/sqb/elements/Game.java
+++ src/sunt/sqb/elements/Game.java
@@ -23,4 +23,24 @@ public class Game {
     public void addJoueurStats(Joueur joueur, TableStatsTypeVehicule stats) {
         this.joueurs.put(joueur, stats);
     }
+
+    /*
+     * Retourne un résumé des statistiques de la game avec une indentation donnée, sous forme d'un String.
+     */
+    public String resumeStatsGame(int indentation) {
+        String indent = "  ".repeat(indentation);
+        String resume = indent + "Game ID: " + id + "\n";
+        resume += indent + "Map: " + map + "\n";
+        resume += indent + "Team Adverse: " + teamAdverse + "\n";
+        resume += indent + "Leader 1: " + leader_1.pseudo() + "\n";
+        resume += indent + "Leader 2: " + leader_2.pseudo() + "\n";
+        resume += indent + "Joueurs Stats:\n";
+        for (Map.Entry<Joueur, TableStatsTypeVehicule> entry : joueurs.entrySet()) {
+            Joueur joueur = entry.getKey();
+            TableStatsTypeVehicule stats = entry.getValue();
+            resume += indent + "  Joueur: " + joueur.pseudo() + "\n";
+            resume += stats.resumeStats(indentation + 2);
+        }
+        return resume;
+    }
 }
